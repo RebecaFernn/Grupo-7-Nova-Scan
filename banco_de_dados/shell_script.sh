74c71933e8c8d28cf4a46056612002b4ad546cef
RebecaFernn <rebeca.santos@sptech.school>
2024-10-09 10:34:51 -0300

    teste para entrar direto para o database

--- banco_de_dados/shell_script.sh
+++ banco_de_dados/shell_script.sh
@@ -23,3 +23,10 @@ sudo docker build -t banco_novascan .
 sudo docker run -d --name bd-novascan -p 3306:3306 banco_novascan
 
 sudo docker exec -it bd-novascan bash
+
+echo "configurando o mysql..."
+
+mysql -u root -purubu100
+USE novaScan;
+
+echo "Bem Vindo ao seu banco!"
