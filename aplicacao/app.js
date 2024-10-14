24dfd4d720752f5badc7983f28ee9ec9a4da85a3
Lucas Canuto <lucas.canuto@sptech.school>
2024-10-14 14:17:17 -0300

    Adicionando arquivos para o caminho de alertas personalizados

--- aplicacao/app.js
+++ aplicacao/app.js
@@ -19,6 +19,7 @@ var indexRouter = require("./src/routes/index");
 var usuarioRouter = require("./src/routes/usuarios");
 var empresasRouter = require("./src/routes/empresas");
 var maquinasRouter = require("./src/routes/maquinas");
+var alertasRouter = require("./src/routes/alertas");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,6 +31,7 @@ app.use("/", indexRouter);
 app.use("/usuarios", usuarioRouter);
 app.use("/empresas", empresasRouter);
 app.use("/maquinas", maquinasRouter);
+app.use("/alertas", alertasRouter);
 app.listen(PORTA_APP, function () {
     console.log(`
     ##   ##  ######   #####             ####       ##     ######     ##              ##  ##    ####    ######  
