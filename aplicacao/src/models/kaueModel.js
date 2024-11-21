8ca81250961abbc0599fdb8ea3e27d945b1daf2f
KaueASouza <kaue.souza@sptech.school>
2024-11-21 13:31:42 -0300

    correção do select

--- aplicacao/src/models/kaueModel.js
+++ aplicacao/src/models/kaueModel.js
@@ -2,13 +2,13 @@ var database = require("../database/config");
 
 function graficopacoterecebido() {
 
-    var instrucaoSql = `SELECT * FROM log WHERE descricao = "BytesRecebidos";`;
+    var instrucaoSql = `SELECT valor FROM log WHERE descricao = "BytesRecebidos";`;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
 function graficopacoteenviado(){
-    var instrucaoSql = `SELECT * FROM log WHERE descricao = "BytesEnviados";`;
+    var instrucaoSql = `SELECT valor FROM log WHERE descricao = "BytesEnviados";`;
     
     console.log("Executando a instrução SQL: \n "+ instrucaoSql);
     return database.executar(instrucaoSql)
